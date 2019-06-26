21213316cad0b9e2792ffdcf3f2eb2b87eccd13f
falseinfinity <alexjgraphics@gmail.com>
2019-06-26 11:36:00 -0400

    Add sign in buttons

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,11 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const authEvents = require('./auth/events')
+
 $(() => {
-  // your JS code goes here
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#change-password').on('submit', authEvents.onChangePassword)
+  $('#sign-out').on('submit', authEvents.onSignOut)
 })
